c914282e45a70e4800efd8ad231e8a3de78a4079
Vignesh Dhanasekaran <v.dhanasekaran0@gmail.com>
2018-05-19 13:27:01 -0400

    Reorganizing some of the settings files

--- powergrid/temp.py
+++ powergrid/temp.py
@@ -3,5 +3,5 @@ import powergrid
 
 if __name__ == '__main__':
 
-    deck = powergrid.PlantDeck('power_plants.csv')
+    deck = powergrid.PlantDeck('settings/power_plants.csv')
     deck.setup_deck()
